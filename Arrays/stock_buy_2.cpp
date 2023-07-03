f89f8b1ee874c3d116d6cc8edb0502943127b7ad
SouvikChoudhury360 <choudhurysouvik2003@gmail.com>
2023-07-03 17:33:20 +0530

    LC 122

---
+++ Arrays/stock_buy_2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int m = prices[0];
+        int out = 0;
+
+        for(int i=1; i<n; i++){
+            if(prices[i]<=m){
+                m = prices[i];
+            }
+            else{
+                out += prices[i] - m;
+                m = prices[i];
+            }
+        }
+
+        return out;
+    }
+};
