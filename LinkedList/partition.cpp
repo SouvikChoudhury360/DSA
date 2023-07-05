6abeee6d3a60cff6c91f332a93464aa6c3ab8d39
SouvikChoudhury360 <choudhurysouvik2003@gmail.com>
2023-07-05 16:34:23 +0530

    LC 86

---
+++ LinkedList/partition.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+class Solution {
+public:
+    
+    ListNode* partition(ListNode* head, int x) {
+        
+        ListNode* small = new ListNode(-1);
+        ListNode* large = new ListNode(-1);
+        ListNode* small_head = small;
+        ListNode* large_head = large;
+        
+        while (head){
+            if (head->val < x){
+                small->next = head;
+                small = small -> next;
+                head = head -> next;
+                small->next = NULL;
+            }
+            else{
+                large->next = head;
+                large = large -> next;
+                head = head -> next;
+                large -> next = NULL;
+            }
+        }
+        small -> next = large_head -> next;
+
+        return small_head -> next;
+    }
+};
+
+// Leetcode 86
+// Make 2 separate ll and add the two partitions to each of them
+// merge them
